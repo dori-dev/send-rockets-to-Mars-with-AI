f73df2381f81dc8a830a0296600d04eaad3afd59
Mohammad Dori <mr.dori.dev@gmail.com>
2021-11-03 13:11:19 +0330

    Add files via upload

--- test_fitness.py
+++ test_fitness.py
@@ -6,19 +6,28 @@ from constant import WIDTH, ROCKET_LIFESPAN
 # dist => 0, WIDTH
 # reached => False, True
 # crashed => False, True
-# alive_tick = 1, ROCKET_LIFESPAN
+# alive_time = 1, ROCKET_LIFESPAN
 
 
-def calculate_fitness(dist, reached, crashed, alive_tick):
+def calculate_fitness(dist, reached, crashed, alive_time):
+    """calculate the fitness,
+    with distance, alive_time, reached and crashed
+    These calculations can be modified to obtain different outputs
+    """
+    # If the distance is greater than WIDTH, it becomes WIDTH
     dist = min(dist, WIDTH)
 
+    # The shorter the distance, the more fitness are calculated
     fitness = WIDTH + 1 - dist
-    fitness *= alive_tick ** 0.5 + 1
+    # Calculating fitness based on when rocket survive
+    fitness *= alive_time ** 0.5 + 1
+    # If the rocket reached or crashed, the fitness changes
     if reached:
-        fitness *= alive_tick ** 0.5 * 2
+        fitness *= alive_time ** 0.5 * 2
     if crashed:
         fitness /= dist ** 0.5 + 1
 
+    # Return the ceiling of x as an Integral.
     fitness = ceil(fitness)
 
     return fitness
